e0931cbaa31aaf2a986ed4de5dca1e868faf7b46
Muhammad Usman <usmanirfan1998@gmail.com>
2025-11-02 21:42:12 +0500

    Added singup controller

--- index.js
+++ index.js
@@ -6,13 +6,23 @@ const PORT = 8000;
 app.use(express.json());
 
 const DIARY = {};
+const EMAILS = new Set();
 
 app.post("/signup", (req, res) => {
   const { name, email, password } = req.body;
 
-  if (email in DIARY) {
+  if (EMAILS.has(email)) {
     res.status(400).json({ error: "Bad email" });
   }
+
+  const token = `${Date.now()}`;
+  DIARY[token] = { name, email, password };
+  EMAILS.add(email);
+
+  res.status(201).json({
+    status: "success",
+    token,
+  });
 });
 
 app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
